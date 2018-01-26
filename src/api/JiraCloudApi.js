10fe1949cce7bb3e7e5248a030fe52cfdf8ff927
dskrodzki <damian.skrodzki@spartez.com>
2018-01-26 16:07:40 +0100

    :sparkles: First version of Jira Cloud API (based on AtlassianService from Canned Responses)

--- src/api/JiraCloudApi.js
+++ src/api/JiraCloudApi.js
@@ -1,4 +1,40 @@
+import extend from 'lodash/extend'
 
-export function get() {
-    // return AP.request(...)
+function ajax(options) {
+    return new Promise((resolve, reject) => {
+        const defaultOptions = {
+            success(response) {
+                resolve(response ? JSON.parse(response) : undefined);
+            },
+
+            error(error) {
+                reject(error);
+            }
+        };
+        const finalOptions = extend(defaultOptions, options);
+
+        AP.request(finalOptions);
+    });
+}
+
+export function put(url, data) {
+    return ajax({
+        type: "PUT",
+        url: url,
+        contentType: "application/json",
+        data: JSON.stringify(data)
+    });
+}
+
+export function post(url, data) {
+    return ajax({
+        type: "POST",
+        url: url,
+        contentType: "application/json",
+        data: JSON.stringify(data)
+    });
+}
+
+export function get(url) {
+    return ajax({url});
 }
