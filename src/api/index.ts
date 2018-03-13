316d6e01e48171a19319f2029eff598e95c1247c
dskrodzki <damian.skrodzki@spartez.com>
2018-03-13 17:35:48 +0100

    :sparkles: Add first version of Jira types for even better autocompletion

--- src/api/index.ts
+++ src/api/index.ts
@@ -19,37 +19,34 @@ function detectApi(): JiraApiBase {
 export default class JiraApi {
     private api = detectApi();
 
-    getProject(projectKeyOrId: string): Promise<object> {
+    getProject(projectKeyOrId: string): Promise<Jira.Project> {
         return this.api.isMock
             ? JiraMocksApi.getProject(projectKeyOrId)
             : this.api.get(`/rest/api/2/project/${projectKeyOrId}`);
     }
 
-    getProjects(): Promise<Array<object>> {
-        const projects = this.api.isMock
+    getProjects(): Promise<Array<Jira.Project>> {
+        return this.api.isMock
             ? JiraMocksApi.getProjects()
             : this.api.get('/rest/api/2/project');
-        return projects as Promise<Array<object>>;
     }
 
-    getUser(userKey: string): Promise<object> {
+    getUser(userKey: string): Promise<Jira.User> {
         return this.api.isMock
             ? JiraMocksApi.getUser(userKey)
             : this.api.get(`/rest/api/2/user?key=${userKey}`);
     }
 
-    getUsers(username: string): Promise<Array<object>> {
-        let users = this.api.isMock
+    getUsers(username: string): Promise<Array<Jira.User>> {
+        return this.api.isMock
             ? JiraMocksApi.getUsers(username)
             : this.api.get(`/rest/api/2/user/search?username=${username}`);
-        return users as Promise<Array<object>>;
     }
 
     getGroupsForPicker({query}: { query: string }) {
-        let users = this.api.isMock
+        return this.api.isMock
             ? JiraMocksApi.getGroupsForPicker(query)
             : this.api.get(`/rest/api/2/groups/picker?query=${query}`);
-        return users as Promise<Array<object>>;
     }
 
     getIssueCreateMeta(): Promise<object> {
---
+++ src/api/jiraTypes.d.ts
@@ -0,0 +1,141 @@
+declare namespace Jira {
+
+    interface UserLight {
+        self: string;
+        name: string;
+        displayName: string;
+        active: boolean;
+    }
+
+    interface User {
+        self: string;
+        name: string;
+        displayName: string;
+        active: boolean;
+        key: string;
+        accountId?: string;
+        emailAddress: string;
+        avatarUrls: AvatarUrls;
+        timeZone: string;
+        groups: Groups;
+        applicationRoles: ApplicationRoles;
+    }
+
+    interface Users {
+        size: number;
+        items: Array<User | UserLight>
+    }
+
+    interface GroupLight {
+        self: string;
+        name: string;
+    }
+
+    interface Group {
+        self: string;
+        name: string;
+        users: Users;
+    }
+
+    interface Groups {
+        size: number;
+        items: Array<Group | GroupLight>;
+    }
+
+    interface ApplicationRoleLight {
+        self: string;
+        name: string;
+    }
+
+    interface ApplicationRole {
+        self: string;
+        key: string,
+        groups: Array<string>,
+        name: string;
+        defaultGroups: Array<string>,
+        selectedByDefault: boolean,
+        defined: boolean,
+        numberOfSeats: number,
+        remainingSeats: number,
+        userCount: number,
+        userCountDescription: number,
+        hasUnlimitedSeats: boolean,
+        platform: boolean
+    }
+
+    interface ApplicationRoles {
+        size: number;
+        items: Array<ApplicationRole | ApplicationRoleLight>;
+    }
+
+    interface Component {
+        self: string;
+        id: string;
+        name: string;
+        description: string;
+        lead: User;
+        assigneeType: string;
+        assignee: User;
+        realAssigneeType: string;
+        realAssignee: User;
+        isAssigneeTypeValid: boolean;
+        project: string;
+        projectId: number;
+    }
+
+    interface AvatarUrls {
+        '48x48': string;
+        '24x24': string;
+        '16x16': string;
+        '32x32': string;
+    }
+
+    interface ProjectCategory {
+        self: string;
+        id: string;
+        name: string;
+        description: string;
+    }
+
+    interface IssueType {
+        self: string;
+        id: string;
+        description: string;
+        iconUrl: string;
+        name: string;
+        subtask: boolean;
+        avatarId: number;
+    }
+
+    interface Version {
+        self: string;
+        id: string;
+        description: string;
+        name: string;
+        archived: boolean;
+        released: boolean;
+        releaseDate: string;
+        overdue: boolean;
+        userReleaseDate: string;
+        projectId: number;
+    }
+
+    interface Project {
+        self: string;
+        id: string;
+        key: string;
+        description: string;
+        lead: User;
+        components: Array<Component>;
+        issueTypes: Array<IssueType>;
+        url: string;
+        email: string;
+        assigneeType: string;
+        versions: Array<Version>;
+        name: string;
+        roles: object;
+        avatarUrls: AvatarUrls;
+        projectCategory: ProjectCategory;
+        simplified: boolean;
+    }
+}
