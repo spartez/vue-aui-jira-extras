0d828b2ec35cd71afae745e08dad5dd1f90c3017
dskrodzki <damian.skrodzki@spartez.com>
2018-03-13 11:54:03 +0100

    :sparkles: Fix global typings declarations for Window and Vue

--- src/api/types.d.ts
+++ src/api/types.d.ts
@@ -1,12 +1,12 @@
 import JiraApi from './index'
 
-interface Window {
-    AP: any;
-    AJS: any;
-    JIRA: any;
-}
+declare global {
+    interface Window {
+        AP: any;
+        AJS: any;
+        JIRA: any;
+    }
 
-declare module 'vue/types/vue' {
     interface Vue {
         $jira: JiraApi;
     }
